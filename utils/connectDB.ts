8f15abed9b834bcf2cd26fd6831cf0fac85361c2
unknown <thecodingpastor@gmail.com>
2023-06-26 14:04:11 +0100

    testing envs on aws

--- utils/connectDB.ts
+++ utils/connectDB.ts
@@ -6,14 +6,6 @@ const connectDB = async () => {
       ? process.env.LOCAL_MONGO_URI
       : process.env.LIVE_MONGO_URI;
 
-  console.log("======================================================");
-  console.log(process.env.LIVE_MONGO_URI);
-  console.log(process.env.NODE_ENV);
-  console.log(process.env.GOOGLE_CAPTCHA_SECRET_KEY);
-  console.log(process.env.CLOUDINARY_CLOUD_NAME);
-  console.log(process.env.LIVE_SITE);
-  console.log("======================================================");
-
   // mongoose.set("strictQuery", false);
   return mongoose
     .connect(connectionString!)
